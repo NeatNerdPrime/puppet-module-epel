211dbe9df3b974265c354636cd262492cc038d04
Evgeni Golov <evgeni@golov.de>
2026-03-10 08:57:17 +0100

    Use "an idempotent resource" example from voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -7,16 +7,6 @@ configure_beaker do |host|
   host.install_package('gnupg2') if fact('os.release.major').to_i >= 10
 end
 
-shared_examples 'an idempotent manifest' do
-  it 'applies with no errors' do
-    apply_manifest(pp, catch_failures: true)
-  end
-
-  it 'applies a second time without changes' do
-    apply_manifest(pp, catch_changes: true)
-  end
-end
-
 shared_examples 'EPEL is available' do
   describe command('/usr/bin/dnf config-manager --dump epel') do
     its(:exit_status) { is_expected.to eq 0 }
